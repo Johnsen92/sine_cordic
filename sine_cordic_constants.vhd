7fdb7d4a6a0f428e50e26c1276aedbe7aed752d2
Jonas Ferdigg <jonas.ferdigg@gmail.com>
2017-03-29 01:19:49 +0200

    sine_cordic_constants.vhd added

---
+++ sine_cordic_constants.vhd
@@ -0,0 +1,8 @@
+package sine_cordic_constants is
+
+	constant INPUT_DATA_WIDTH    : integer := 8;
+    	constant OUTPUT_DATA_WIDTH   : integer := 8;
+    	constant ITERATION_COUNT     : integer := 16;
+    	constant INTERNAL_PRECISION  : integer := 8;
+
+end sine_cordic_constants;
