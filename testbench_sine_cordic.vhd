cef4fd05715ff1c24c5b1cbe51ab2de545950a01
Luke Maher <luke.maher@gmail.com>
2017-04-05 00:44:28 +0200

    some cleanup, INTERNAL_DATA_WIDTH added NOTE: precalculated constants seem to only be precise to the fourth or so decimal place (decimal, not binary!)

--- testbench_sine_cordic.vhd
+++ testbench_sine_cordic.vhd
@@ -26,12 +26,11 @@ architecture beh of testbench is
         );
     end component;
 
-    constant CLK_PERIOD : time := 4 ns;
+    constant CLK_PERIOD : time := 2 ps;
     constant INPUT_DATA_WIDTH : integer := 8;
     constant OUTPUT_DATA_WIDTH : integer := 8;
     constant ITERATION_COUNT : integer := 12;
-    
-    type testcase_array is array(8 downto 0) of real;
+    type testcase_array is array(10 downto 0) of real;
     constant testcases : testcase_array := (
         0.0,
         MATH_PI*2.0**(-1),
@@ -40,10 +39,10 @@ architecture beh of testbench is
         -MATH_PI*3.0**(-1),
         MATH_PI*4.0**(-1),
         -MATH_PI*4.0**(-1),
+        MATH_PI*6.0**(-1),
+        MATH_PI*6.0**(-1),
         MATH_PI,
         -MATH_PI
-        --MATH_PI*6.0**(-1),
-        ---MATH_PI*6.0**(-1)
     );
 
     signal clk, reset : std_logic;
